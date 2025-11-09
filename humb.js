a7ce348553c6c79b5ce154e8104cb9bf271bc160
ClarkClark1213 <Clarkjohnbucog13@gmail.com>
2025-11-09 17:02:30 +0800

    Update humb.js

--- humb.js
+++ humb.js
@@ -18,3 +18,19 @@ tailwind.config = {
 }
 
 
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
