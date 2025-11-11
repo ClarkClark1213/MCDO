e0df6eafa21dc0986bb8d27fa15b678cecc2c60c
ClarkClark1213 <Clarkjohnbucog13@gmail.com>
2025-11-12 05:46:57 +0800

    Update humb.js

--- humb.js
+++ humb.js
@@ -1,36 +1,7 @@
-
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
 
 burger.onclick = () => {
-  burger.classList.toggle('open');
-  menu.classList.toggle('hidden');
+    burger.classList.toggle('open');
+    menu.classList.toggle('hidden');
 };
-
-tailwind.config = {
-  theme: {
-    extend: {
-      fontFamily: {
-        pantograph: ['PantographPro', 'sans-serif'],
-      }
-    }
-  }
-}
-
-
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
